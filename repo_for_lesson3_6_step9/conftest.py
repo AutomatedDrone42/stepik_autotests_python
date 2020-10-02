8485fa29a6e2404b4b99a253012c63816fc8bd93
Sasha Li <automateddrone42@gmail.com>
2020-10-02 13:08:49 +0300

    Lesson3 upd4

--- repo_for_lesson3_6_step9/conftest.py
+++ repo_for_lesson3_6_step9/conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language: ru, en, fr, <...>")
+                     help="Choose language: ru, en, fr ...")
 
 
 @pytest.fixture(scope="function")
@@ -16,14 +16,15 @@ def browser(request):
     user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
         options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_experimental_option('prefs',
+                                        {'intl.accept_languages': user_language})
+        print("\nStart chrome browser for test..")
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
+        print("\nStart firefox browser for test..")
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
