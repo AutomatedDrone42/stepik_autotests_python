0458824841e981cec85d278a82a2c9934159f3de
Sasha Li <automateddrone42@gmail.com>
2020-10-07 18:53:00 +0300

    Lesson3 upd5

--- repo_for_lesson3_6_step9/test_items.py
+++ repo_for_lesson3_6_step9/test_items.py
@@ -4,5 +4,3 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_button_add_to_basket_is_present(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector("button.btn-add-to-basket")
-
-# Cannot find add to basket button"
